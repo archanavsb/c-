46abfa9063e937ba94e562360c0f6d9509e1fa4b
archanavsb <30317021+archanavsb@users.noreply.github.com>
2017-07-21 12:20:27 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter a character");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch='o'||ch=='U')
+printf("%ch is vowel \n",ch);
+else
+printf("%ch is not a vowel \n",ch);
+return 0;
+}
